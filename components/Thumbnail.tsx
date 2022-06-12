604a2b536e86a3ad0a386308dce05254b60fabc5
minhhhnghiaaa <ohmybaby1999@gmail.com>
2022-06-12 23:34:10 +0700

    day1 completed

---
+++ components/Thumbnail.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+import { baseThumnailUrl } from "../constants/movie"
+import { Movie } from "../typings"
+
+interface Props { 
+  movie: Movie,
+}
+function Thumbnail({ movie }: Props) {
+  return (
+    <div className="relative h-28 min-w-[180px] cursor-pointer transition 
+      duration-200 ease-in-out md:h-36 md:min-w-[260px] md:hover:scale-105">
+      <Image 
+        src={`${baseThumnailUrl}${movie.backdrop_path || movie.poster_path}`}
+        className="rounded-sm object-cover md:rounded"
+        layout="fill"  
+        alt="poster" />
+    </div>
+  )
+}
+
+export default Thumbnail
